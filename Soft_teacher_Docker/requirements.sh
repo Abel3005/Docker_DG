41d88b7cdcd920e0e2e45e744bad0a84061b3c9d
kimdaegun <kimdaegun1114@gmail.com>
2021-11-27 18:26:02 +0900

    soft_teacher_docker

--- Soft_teacher_Docker/requirements.sh
+++ Soft_teacher_Docker/requirements.sh
@@ -1,24 +1,6 @@
-#!bin/bash
-
-cd /root/ws
-pip3 install numpy && pip3 install cython && pip3 install -r requirement.txt
-cd /root/
-git clone https://github.com/NVIDIA/apex
-git clone https://github.com/hfutcgncas/normalSpeed.git
-git clone https://github.com/pybind/pybind11.git
-#
-
-#apex install
-cd /root/apex
-git reset --hard 3fe10b5597ba14a748ebb271a6ab97c09c5701ac
-pip3 install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-#pybind install
-cd /root/pybind11
-mkdir build
-cd build
-cmake ../
-make -j8
-make install
-#normalSpeed
-cd /root/normalSpeed/normalSpeed
-python3 setup.py install --user
+conda install -y pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+conda install -c anaconda msgpack-python 
+conda install -y numpy
+pip install mim
+mim install mmdet
+pip install mmdet
